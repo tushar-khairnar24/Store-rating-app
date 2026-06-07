2c10319cb0f73116cec497b0bbb5dc3b64e92469
Tushar Khairnar <tusharkhairnar247@gmail.com>
2026-06-07 13:31:29 +0530

    Update axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://store-rating-app-eyfn.onrender.com",  
+  baseURL: "https://store-rating-app-eyfn.onrender.com/api",  
 });
 
 api.interceptors.request.use((config) => {
